35a9bb8e7ce829023c0b14514080f0606fe71c14
firmread <tothongc@box784.bluehost.com>
2014-01-05 08:07:35 -0700

    first commit

---
+++ collab/index.php
@@ -0,0 +1,27 @@
+
+<?php include "../php/php/header.php"?>
+
+<section class="container_12">
+	<div class="grid_12">
+		<h1>code:ocean of the air</h1>
+		<p>"Think of all your movements in life as dance movements.<br>
+		 Breathe. <br>
+		Watch. <br>
+		Listen. <br>
+		Touch. <br>
+		And move between the earth and the sky." <br>
+		<br>
+		Yoko Ono
+		<h2>11 August at 13:33 via Twitter</h2></p>
+	</div>
+</section>
+<br>
+
+<section class="container_12">
+	<div class="grid_12">
+		<a href="#"><img src="../media/drawn.jpg" alt="code:Ocean air"></a>
+	</div>
+</section>
+
+<?php include "../php/php/archive.php"?>
+<?php include "../php/php/footer.php"?>
---
+++ php/footer.php
@@ -0,0 +1,31 @@
+
+<footer class="container_12">
+	
+	<nav class="grid_8">
+		<ul>
+			<li>
+				<a href="indi">individual</a>
+			</li>
+			<li>
+				<a href="collab" class="color2">collaborate</a>
+			</li>
+			<li>
+				<a href="http://blog.litchirhythm.com/" class="color3">blog</a>
+			</li>
+			<li>
+				<a href="http://www.litchirhythm.com/resume/" class="color4">cv</a>
+			</li>
+		</ul>
+	</nav>
+
+	<div class="grid_4">
+		<a href="#">
+		<h1>firm</h1>
+		<h2>THARIT TOTHONG</h2>
+		</a>
+	</div>
+</footer>
+
+
+</body>
+</html>
